4537d5fdab6822baadb39205d714c17b36adf1c4
louis <louis-rogeon@outlook.fr>
2018-01-11 11:19:25 +0100

    Add EtatCoffre and implement coffre

---
+++ src/EtatCoffre.java
@@ -0,0 +1,21 @@
+
+public abstract class EtatCoffre {
+	
+	public void oterLivre (Coffre coffre) {}
+	
+	public void remettreLivre (Coffre coffre) {}
+	
+	public void tournerChandelleVersDroite (Coffre coffre) {}
+	
+	public void tournerChandelleVersGauche (Coffre coffre) {}
+	
+	public void fermerCoffre (Coffre coffre) {}
+	
+	public void lapinEstLibere (Coffre coffre) {}
+	
+	public void chienEstLibere (Coffre coffre) {}
+	
+	public String nomEtat () {
+		return this.getClass().toString();
+	}
+}
